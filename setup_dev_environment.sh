3da88731658340dc8b09ff3c261f9d7b7ce2e62f
ben <ben@nerdlabor.de>
2011-04-03 14:05:25 +0200

    added setup script

---
+++ setup_dev_environment.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+########################################
+#
+# This is a script for setting up
+# a complete development environment
+# of the API Leipzig.
+#
+# ONLY USE THIS FOR *LOCAL* DEVELOPMENT!
+#
+# Usage:
+#  - Install git, rvm and bundler
+#  - Copy the script in the directory
+#    where you want to create the api
+#    folder
+#  - run the script
+#
+########################################
+
+echo "cloning all repositories..."
+git clone git@github.com:benben/frontend.git api/
+cd api/
+git clone git@github.com:benben/panel.git
+git clone git@github.com:benben/api.git
+git clone git@github.com:benben/instiki.git wiki/
+
+echo "bundle everything..."
+cd panel
+bundle
+cd ../api
+bundle
+cd ../wiki
+bundle
+cd ..
+
+echo "generating dev files for the api..."
+cd api/
+echo -e "adapter: sqlite3\ndatabase: db/development.sqlite3\npool: 5\ntimeout: 5000" > database.yml
+rake db:migrate
+rake db:seed
+rake permissions:init
+cd ..
+
+echo "generating dev files for the panel..."
+cd panel/
+echo -e "development:\n  adapter: sqlite3\n  database: ../api/db/development.sqlite3\n  pool: 5\n  timeout: 5000" > config/database.yml
+cd ..
+cp -R css/ panel/public/css
+cp -R js/ panel/public/js
+cp -R images/* panel/public/images/
+
+echo "generating dev files for the wiki..."
+cd wiki/
+cp config/database_example.yml config/database.yml
+rake db:migrate
+cd ..
+cp -R css/ wiki/public/css
+cp -R js/ wiki/public/js
+cp -R images/* wiki/public/images/
+
